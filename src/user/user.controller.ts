ef82e5e0e14d64090b8d117da2df6dee5fd46de0
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2023-05-08 15:21:53 +0200

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get(':id')
+  findAll(@Param('id') id: string) {
+    return { user: { id: id } };
+  }
+
+  @Post()
+  create(@Body('name') name: string) {
+    return 'the username is ' + name;
+  }
+}
